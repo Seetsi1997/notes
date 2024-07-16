f15c9a2dc39e3e2ec043b5a12800b72fd8d778f6
Ganeth <83027242+Seetsi1997@users.noreply.github.com>
2024-07-17 00:04:42 +0200

    commit

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-# Notepad app
+# portfolio ##
 
-Notes is a simple text editor. It's ideal for composing brief text documents that you want to save as plain text. If you've never used Notepad before, you might be surprised how simple it is to use.
+My name is Ganeth Seetsi, and i can showcase my technical abilities and services through software development, web development, technical programming and database.
 
-# portfolio
+<!-- Responsive Personal Portfolio -->
+. Key Features.
 
-My name is Ganeth Seetsi, and I can demonstrate my technical skills and services in software development, web development, technical programming, and database. {My experience includes C#, Dart, SQL Server, Flutter, Backendless, and Firebase.}.
+- Responsive personal portfolio website using HTML CSS and JavaScript.
+- Smooth scrolling in each section.
+- Including light or dark mode.
+- Sending email in the contact section.
+- All mobile devices are compatible, and the user interface is stylish and pleasant.
+
+<!-- created Todo App using Flutter Dart Language -->
+. Todo App
+
+- Todo App is a new Flutter project that requires new users to register an account. After creating an account, users can  login. They will be able to create, modify, and delete tasks.  
+
+<!-- created a Calculator App using HTML CSS JavaScript -->
+. Calculator App
+
+- A simple Calculator app with fundamental mathematical operators.
+
+<!-- created Notes App using HTML CSS JavaScript -->
+. Note App
+
+- Notes is a simple text editor. It's ideal for composing brief text documents that you want to save as plain text. If you've never used Notepad before, you might be surprised how simple it is to use. They will be able to edit and delete task notes.
+
+<!-- created Weather App with HTML CSS JavaScript -->
+. Weather App
+
+- Weather is the state of the atmosphere, comprising temperature, air pressure, wind, humidity, cloud cover, rain.
---
+++ js/script.js
@@ -0,0 +1,121 @@
+const addBox = document.querySelector(".add-box"),
+popupBox = document.querySelector(".popup-box"),
+popupTitle = popupBox.querySelector("header p"),
+closeIcon = popupBox.querySelector("header i"),
+titleTag = popupBox.querySelector("input"),
+descTag = popupBox.querySelector("textarea"),
+addBtn = popupBox.querySelector("button");
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+              "Aug", "Sept", "Oct", "Nov", "Dec"];
+const days = ['Sun','Mon','Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+let isUpdate = false, updateId;
+
+addBox.addEventListener("click", () => {
+    popupTitle.innerText = `Add New Note`;
+    addBtn.innerText = `Add`;
+    popupBox.classList.add("show");
+    document.querySelector("body").style.overflow = "hidden";
+    if(window.innerWidth > 660) {
+        titleTag.focus();
+    }
+});
+
+closeIcon.addEventListener("click", () => {
+    isUpdate = false;
+    titleTag.value = descTag.value = "";
+    popupBox.classList.remove("show");
+    document.querySelector("body").style.overflow = "auto";
+});
+
+function showNotes() {
+    if(!notes) return;
+    document.querySelectorAll(".note").forEach(li => li.remove());
+    notes.forEach((note, id) => {
+        let filterDesc = note.description.replaceAll("\n", '<br/>');
+        let liTag = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${filterDesc}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="menu">
+                                    <li onclick="updateNote(${id}, '${note.title}', '${filterDesc}')"><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+showNotes();
+
+function showMenu(elem) {
+    elem.parentElement.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != elem) {
+            elem.parentElement.classList.remove("show");
+        }
+    });
+}
+
+function deleteNote(noteId) {
+    let confirmDel = confirm("Are you sure you want to delete Note?");
+    if(!confirmDel) return;
+    notes.splice(noteId, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+}
+
+function updateNote(noteId, title, filterDesc) {
+    let description = filterDesc.replaceAll('<br/>', '\r\n');
+    updateId = noteId;
+    isUpdate = true;
+    addBox.click();
+    titleTag.value = title;
+    descTag.value = description;
+    popupTitle.innerText = "Update Note";
+    addBtn.innerText = "Update";
+}
+
+addBtn.addEventListener("click", e => {
+    e.preventDefault();
+    let title = titleTag.value.trim(),
+    description = descTag.value.trim();
+
+    if(title || description) {
+        let currentDate = new Date(),
+        weekday = days[currentDate.getDay()],
+        day = currentDate.getDate(),
+        month = months[currentDate.getMonth()],
+        year = currentDate.getFullYear(),
+        hour = (currentDate.getHours() < 10 ? "0" : "") + currentDate.getHours(),
+        minutes = (currentDate.getMinutes() < 10 ? "0" : "") + currentDate.getMinutes();
+
+        let noteInfo = {title, description, date: `${weekday}, ${day} ${month}, ${year} - ${hour}:${minutes}`}
+
+        if(!isUpdate) {
+            notes.push(noteInfo);
+        } else {
+            isUpdate = false;
+            notes[updateId] = noteInfo;
+        }
+        localStorage.setItem("notes", JSON.stringify(notes));
+        showNotes();
+        closeIcon.click();
+    }
+    
+});
+
+function dateTime()
+{
+    const currentDate = new Date();
+    document.getElementById('year').innerText = currentDate.getUTCFullYear();
+    
+}
+dateTime();
